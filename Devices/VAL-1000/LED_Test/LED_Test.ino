f43a950246f9e92208cd1e07d1696bed3b0805b3
Daniel Frenkel <daniel@valarsystems.com>
2022-03-26 20:42:26 -0700

    Update LED_Test.ino

--- Devices/VAL-1000/LED_Test/LED_Test.ino
+++ Devices/VAL-1000/LED_Test/LED_Test.ino
@@ -6,7 +6,8 @@
 int brightness0 = 0;    // how bright the LED is
 int brightness1 = 0;    // how bright the LED is
 int brightness2 = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
+int fade0Amount = 15;    // how many points to fade the LED by
+int fade1Amount = 15; 
 
 int btn1Press;
 int btn2Press;
@@ -28,14 +29,12 @@ void IRAM_ATTR button2pressed()
 void setup() {
 
   Serial.begin(115200);
-  ledcAttachPin(LED1, 0); // assign a led pins to a channel
-  ledcAttachPin(LED2, 1); // assign a led pins to a channel
-  ledcAttachPin(LED1, 2); // assign a led pins to a channel
-  ledcAttachPin(LED2, 2); // assign a led pins to a channel
-  
+  ledcAttachPin(LED1, 1); // assign a led pins to a channel
+  ledcAttachPin(LED2, 0); // assign a led pins to a channel
+
   ledcSetup(0, 5000, 8); // 12 kHz PWM, 8-bit resolution
   ledcSetup(1, 5000, 8); // 12 kHz PWM, 8-bit resolution
-  ledcSetup(2, 5000, 8); // 12 kHz PWM, 8-bit resolution
+
 
   pinMode(BUTTON1,INPUT_PULLUP );
   pinMode(BUTTON2,INPUT_PULLUP );
@@ -43,82 +42,85 @@ void setup() {
   pinMode(LED2,OUTPUT);
   attachInterrupt(BUTTON1, button1pressed, FALLING);
   attachInterrupt(BUTTON2, button2pressed, FALLING);
-  
-}
 
-void led1On() {
-    
-    while (brightness0 < 255) { //Blocks
-      ledcWrite(0, brightness0); // set the brightness of the LED
-      brightness0 = brightness0 + fadeAmount;
-      vTaskDelay(30);
-    }
-    brightness0 = 0;
-    ledcWrite(0, brightness0); // set the brightness of the LED
-}
-
-void led2On() {
-    
-    while (brightness1 < 255) { //Blocks
-      ledcWrite(1, brightness1); // set the brightness of the LED
-      brightness1 = brightness1 + fadeAmount;
-      vTaskDelay(30);
-    }
-    brightness1 = 0;
-    ledcWrite(1, brightness1); // set the brightness of the LED
-}
-
-void ledBothOn() {
-    Serial.println("Turn both LEDS on");
-    brightness2 = 0;
-    
-    while (brightness2 < 255) { //Blocks
-      ledcWrite(2, brightness2); // set the brightness of the LED
-      brightness2 = brightness2 + fadeAmount;
-      vTaskDelay(30);
-    }
-    
+  ledcWrite(0, 0); // turn off LED 
+  ledcWrite(1, 0); // turn off LED 
 
 }
+
+
+int button1Timer;
+int button2Timer;
+int waitButton1Timer;
+int waitButton2Timer;
+bool motorRunning;
+
 
 void loop() {
 
-  Serial.println(digitalRead(BUTTON2));
-  
   if(btn1Press == 1) {
-    Serial.println("BUTTON 1 PRESS");
-    led1On();
-    int timeMax1 = millis() + 1000;
-    while (millis() < timeMax1){ 
-      if(btn2Press == 1){
-             Serial.println("CLOSE DEVICE");
-             ledBothOn();
-             
-             }
-    }
-    btn1Press = 0;
-    btn2Press = 0;
-  }
+    //Serial.println("BUTTON 1 PRESS");
+    button1Timer = millis();
+    waitButton2Timer = millis() + 1000;
+    
+    if (millis() < waitButton2Timer && btn2Press == 1 && motorRunning == false) {
+      Serial.println("START MOTOR CLOSE");
+      motorRunning = true;
+      }
+    
+    if (brightness0 <= 255 && brightness0 >= 0) { 
+      ledcWrite(0, brightness0); // set the brightness of the LED
+      brightness0 = brightness0 + fade0Amount;
+      vTaskDelay(30);
+      }
 
-  /*
-  if (btn2Press == 1) {
-    
-    Serial.println("START BUTTON 2 PRESS");
-    int timeMax2 = millis() + 1000;
-    
-    while (millis() < timeMax2){   
-      if(btn1Press == 1){
-             Serial.println("OPEN DEVICE");
-             ledBothOn();
-    
-             } 
+    //Fade instead of turn off
+    if (brightness0 > 255) {
+      brightness0 = 255;
+      fade0Amount = -fade0Amount;
+      }
+
+     if (brightness0 < 0){
+      
+      brightness0 = 0;
+      ledcWrite(0, brightness0);
+      btn1Press = 0;
+      fade0Amount = 15;
+      motorRunning = false;
      }
-     
-     btn1Press = 0;
-     btn2Press = 0;
-     Serial.println("END BUTTON 2 PRESS");
-   }
-
-  */
+    }
     
+  if (btn2Press == 1) {
+    //Serial.println(" BUTTON 2 PRESS");
+    button2Timer = millis();
+    waitButton1Timer = millis() + 1000;
+    
+    if (millis() < waitButton1Timer && btn1Press == 1 && motorRunning == false ) {
+      Serial.println("START MOTOR OPEN");
+      motorRunning = true;
+      }
+      
+    if (brightness1 <= 255 && brightness1 >= 0) { 
+      ledcWrite(1, brightness1); // set the brightness of the LED
+      brightness1 = brightness1 + fade1Amount;
+      vTaskDelay(30);
+      }
+
+    //Fade instead of turn off
+    if (brightness1 > 255) {
+      brightness1 = 255;
+      fade1Amount = -fade1Amount;
+      }
+
+     if (brightness1 < 0){
+      
+      brightness1 = 0;
+      ledcWrite(1, brightness1);
+      btn2Press = 0;
+      fade1Amount = 15;
+      motorRunning = false;
+     }
+
+  }
+   
 }
