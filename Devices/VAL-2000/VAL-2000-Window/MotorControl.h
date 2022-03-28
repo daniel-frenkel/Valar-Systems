8f1feb8625f1b7e5baa0ed44f02cbb2cb9706428
Daniel Frenkel <daniel@valarsystems.com>
2022-03-27 20:18:30 -0700

    Updates

---
+++ Devices/VAL-2000/VAL-2000-Window/MotorControl.h
@@ -0,0 +1,238 @@
+uint16_t positionLabel;
+
+#include "soc/timer_group_struct.h"
+#include "soc/timer_group_reg.h"
+
+#define STEP_PIN  13
+#define DIR_PIN  14
+#define ENABLE_PIN 27
+#define BUTTON1 23
+#define BUTTON2 34
+#define RXD2 16
+#define TXD2 17
+#define STALLGUARD 2 
+#define SENSOR1 32
+#define SENSOR2 22
+#define LED1 19
+#define LED2 22
+
+#define SHAFT true
+
+#define SERIAL_PORT_2    Serial2    // TMC2208/TMC2224 HardwareSerial port
+#define DRIVER_ADDRESS   0b00       // TMC2209 Driver address according to MS1 and MS2
+#define R_SENSE          0.10f      // E_SENSE for current calc.  
+
+
+//AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+FastAccelStepperEngine engine = FastAccelStepperEngine();
+FastAccelStepper *stepper = NULL;
+
+TMC2209Stepper driver(&SERIAL_PORT_2, R_SENSE , DRIVER_ADDRESS);
+
+void IRAM_ATTR button1pressed() 
+{
+    move_to_step = 0;
+    //run_motor = true;
+    btn1Press = 1;
+}
+
+void IRAM_ATTR button2pressed() 
+{
+    move_to_step = max_steps;
+    //run_motor = true;
+    btn2Press = 1;
+}
+
+void IRAM_ATTR stalled_position()
+{
+  stalled_motor = true;
+
+}
+
+void IRAM_ATTR sensor_short()
+{
+  sensor2_trip = true;
+}
+
+void IRAM_ATTR sensor_long()
+{
+  sensor1_trip = true;
+}
+
+void IRAM_ATTR wifi_button_press()
+{
+  wifi_button = true;
+}
+
+void move_motor() {
+  Serial.print("Current Position: ");
+  Serial.println(current_position);
+  
+  Serial.print("Moving to Position: ");
+  Serial.println(move_to_step);
+
+  stepper->setCurrentPosition(current_position);
+  //stepper->moveTo(move_to_step);
+  
+  stalled_motor = false;
+  sensor1_trip = false;
+  sensor2_trip = false;
+  
+  stepper->setAcceleration(accel);
+  stepper->setSpeedInHz(max_speed);
+  driver.rms_current(current);
+  driver.SGTHRS(stall);
+  driver.TCOOLTHRS(tcools);
+
+if(move_to_step == 0)
+{
+  
+  Serial.println("Closing until Trip");
+  stepper->moveTo(-10000);
+
+      while (stepper->getCurrentPosition() != stepper->targetPos()) 
+      {
+          if (sensor1_trip == true || (digitalRead(SENSOR1) == LOW)) 
+          {
+            printf("TRIPPED ON 1\n");
+            stepper->forceStop(); // Stop as fast as possible: sets new target
+            stepper->setCurrentPosition(0);
+            stepper->moveTo(0);
+          }
+          
+          if (digitalRead(STALLGUARD) == HIGH)
+          {
+            printf("Stalled\n");
+            stepper->forceStop(); // Stop as fast as possible: sets new target
+            stepper->moveTo(stepper->getCurrentPosition());
+          }
+
+      vTaskDelay(1);
+      }
+  }
+  else if(move_to_step > current_position) // Open
+  {
+
+      Serial.println("Opening");
+      stepper->moveTo(move_to_step);
+      //stepper.enableOutputs();
+    
+      while (stepper->getCurrentPosition() != stepper->targetPos()) {
+
+      if (sensor2_trip == true || digitalRead(SENSOR2) == LOW)
+      {
+        printf("TRIPPED ON 2\n");
+        stepper->forceStop(); // Stop as fast as possible: sets new target
+        stepper->setCurrentPosition(max_steps);
+        stepper->moveTo(max_steps);
+      }
+      
+      if (stalled_motor == true)
+      {
+        printf("Stalled\n");
+        stepper->forceStop(); // Stop as fast as possible: sets new target
+        stepper->moveTo(stepper->getCurrentPosition());
+      }
+
+      vTaskDelay(1);
+
+      }
+  }
+  else if(move_to_step < current_position)
+  {
+  
+      Serial.println("Closing");
+      stepper->moveTo(move_to_step);
+      //stepper.enableOutputs();
+    
+      while (stepper->getCurrentPosition() != stepper->targetPos()) 
+      {
+        
+          if (sensor1_trip == true ||(digitalRead(SENSOR1) == LOW)) //(sensor1_trip == true) || 
+          {
+            printf("TRIPPED ON 1\n");
+            stepper->forceStop(); // Stop as fast as possible: sets new target
+            stepper->setCurrentPosition(0);
+            stepper->moveTo(0);
+          }
+          
+          if (stalled_motor == true)
+          {
+            printf("Stalled\n");
+            stepper->forceStop(); // Stop as fast as possible: sets new target
+            stepper->moveTo(stepper->getCurrentPosition());
+          }
+
+      vTaskDelay(1);
+      }
+}
+else if(current_position == move_to_step)
+{
+  Serial.println("ALREADY THERE!");
+}
+else
+{
+  Serial.println("DO NOTHING!");
+}
+
+      current_position = stepper->getCurrentPosition();
+      //stepper.disableOutputs();
+      printf("Motor Function Complete\n");
+}
+
+// put your setup code here, to run once:
+void setup_motors(){ 
+  
+  pinMode(ENABLE_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(STALLGUARD ,INPUT);
+  pinMode(WIFI_PIN ,INPUT);
+
+  pinMode(SENSOR1,INPUT);
+  pinMode(SENSOR2,INPUT);
+
+  pinMode(BUTTON1,INPUT_PULLUP);
+  pinMode(BUTTON2,INPUT_PULLUP);
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+
+
+  
+  SERIAL_PORT_2.begin(115200);
+
+  driver.begin();
+  driver.toff(4);
+  driver.blank_time(24);
+  driver.I_scale_analog(false);
+  driver.internal_Rsense(false);
+  driver.mstep_reg_select(true);
+  driver.rms_current(current); 
+  driver.SGTHRS(stall);
+  driver.microsteps(0);
+  driver.TCOOLTHRS(tcools); // 
+  driver.TPWMTHRS(0);
+  driver.semin(0);
+  driver.shaft(SHAFT);  
+  driver.en_spreadCycle(false);
+  driver.pdn_disable(true);
+
+  engine.init();
+  stepper = engine.stepperConnectToPin(STEP_PIN);
+  stepper->setDirectionPin(DIR_PIN);
+  stepper->setEnablePin(ENABLE_PIN);
+  stepper->setAutoEnable(true);
+    
+  //stepper.setEnablePin(ENABLE_PIN);
+  //stepper.setPinsInverted(false, false, true);
+  //stepper.disableOutputs();
+  
+  attachInterrupt(STALLGUARD, stalled_position, RISING);
+  attachInterrupt(WIFI_PIN, wifi_button_press, FALLING);
+  attachInterrupt(BUTTON1, button1pressed, FALLING);
+  attachInterrupt(BUTTON2, button2pressed, FALLING);
+  attachInterrupt(SENSOR1, sensor_long, FALLING);
+  attachInterrupt(SENSOR2, sensor_short, FALLING);
+  
+
+}
---
+++ Devices/VAL-2000/VAL-2000-Window/VAL-2000-Window.ino
@@ -0,0 +1,175 @@
+#include <Arduino.h>
+#include <ESPUI.h>
+#include <ezTime.h>
+//#include <AccelStepper.h>
+#include "FastAccelStepper.h"
+#include <HardwareSerial.h>
+#include <TMCStepper.h>
+#include <SPI.h>
+#include <Preferences.h>
+#include "Memory.h"
+#include "ResetButton.h"
+#include "MotorControl.h"
+#include "API.h"
+#include "calibration.h"
+#include "ESPUI.h"
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  
+  Serial.begin(115200);
+  delay(1000);
+  preferences.begin("local", false);
+  
+  load_preferences();
+  setup_motors();
+  API();
+  ESPUIsetup();
+
+  
+  // Now set up tasks to run independently.
+  xTaskCreatePinnedToCore(
+    OtherTask //Motor Task
+    ,  "OtherTask"   // A name just for humans
+    ,  1024*4  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  3  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL 
+    ,  1);
+
+  xTaskCreatePinnedToCore(
+    MotorTask //Motor Task
+    ,  "MotorTask"   // A name just for humans
+    ,  1024*4  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL 
+    ,  0);
+}
+
+void loop()
+{
+  // Empty. Things are done in Tasks.
+
+}
+
+/*--------------------------------------------------*/
+/*---------------------- Tasks ---------------------*/
+/*--------------------------------------------------*/
+
+void OtherTask(void *pvParameters)  // Motor Task
+{
+  (void) pvParameters;
+
+  for (;;)
+  {
+
+    if(btn1Press == 1) {
+    //Serial.println("BUTTON 1 PRESS");
+    button1Timer = millis();
+    waitButton2Timer = millis() + 1000;
+    
+    if (millis() < waitButton2Timer && btn2Press == 1 && motorRunning == false) {
+      Serial.println("START MOTOR CLOSE");
+      motorRunning = true;
+      move_to_step = -10000;
+      run_motor = true;
+      }
+    
+    if (brightness0 <= 255 && brightness0 >= 0) { 
+      ledcWrite(0, brightness0); // set the brightness of the LED
+      brightness0 = brightness0 + fade0Amount;
+      vTaskDelay(30);
+      }
+
+    //Fade instead of turn off
+    if (brightness0 > 255) {
+      brightness0 = 255;
+      fade0Amount = -fade0Amount;
+      }
+
+     if (brightness0 < 0){
+      
+      brightness0 = 0;
+      ledcWrite(0, brightness0);
+      btn1Press = 0;
+      fade0Amount = 15;
+      motorRunning = false;
+     }
+    }
+    
+  if (btn2Press == 1) {
+    //Serial.println(" BUTTON 2 PRESS");
+    button2Timer = millis();
+    waitButton1Timer = millis() + 1000;
+    
+    if (millis() < waitButton1Timer && btn1Press == 1 && motorRunning == false ) {
+      Serial.println("START MOTOR OPEN");
+      motorRunning = true;
+      move_to_step = max_steps;
+      run_motor = true;
+      }
+      
+    if (brightness1 <= 255 && brightness1 >= 0) { 
+      ledcWrite(1, brightness1); // set the brightness of the LED
+      brightness1 = brightness1 + fade1Amount;
+      vTaskDelay(30);
+      }
+
+    //Fade instead of turn off
+    if (brightness1 > 255) {
+      brightness1 = 255;
+      fade1Amount = -fade1Amount;
+      }
+
+     if (brightness1 < 0){
+      
+      brightness1 = 0;
+      ledcWrite(1, brightness1);
+      btn2Press = 0;
+      fade1Amount = 15;
+      motorRunning = false;
+     }  
+  }
+
+  else
+    {
+      vTaskDelay(1);
+    }
+ }
+}
+
+void MotorTask(void *pvParameters)  // Motor Task
+{
+  (void) pvParameters;
+
+  for (;;)
+  {
+    if(run_motor == true)
+    {
+      Serial.println("Run Motor Function");
+      move_motor();
+      run_motor = false;
+      ESPUI.updateLabel(positionLabel, String(int(((float)current_position/(float)max_steps)*100)));
+      Serial.println("Motor Complete");
+    }
+    else if(calibrate == 1)
+    {
+      TravelDistance();
+      CalibrateCurrent();
+      ESPUI.updateNumber(currentMax, current);
+      CalibrateStall();
+      ESPUI.updateNumber(stallMax, stall);
+      calibrate = 0;
+    }
+    else if(wifi_button == true)
+    {
+      button_change();
+      wifi_button = false;
+    }
+    else
+    {
+      vTaskDelay(1);
+    }
+  }
+}
